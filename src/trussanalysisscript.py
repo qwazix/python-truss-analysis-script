e074c0f462bb4ac44b3638398d206d18fee2e466
Michael Demetriou <qwazix@gmail.com>
2013-01-12 14:55:38 +0200

    json of truss completed

---
+++ src/trussanalysisscript.py
@@ -0,0 +1,13 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="qwazix"
+__date__ ="$Jan 12, 2013 2:52:56 PM$"
+
+class myClass:
+    a = 0;
+
+if __name__ == "__main__":
+    print "Hello World"
+    myObj = myClass();
+    print myObj.a
